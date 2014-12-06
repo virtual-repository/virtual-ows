9c1b50d1f81b9532178af5eb2d6c6c6bcdcc6ae4
fabiosimeoni <fabio.simeoni@me.com>
2014-12-06 11:05:29 +0100

    configuration now aligned to ows scope

--- src/main/java/org/virtual/ows/common/Constants.java
+++ src/main/java/org/virtual/ows/common/Constants.java
@@ -4,8 +4,8 @@ package org.virtual.ows.common;
 
 public class Constants {
 	
-	public static final String config_property = "wfs.config";
-	public static final String config_filename = "wfs.json";
+	public static final String config_property = "ows.config";
+	public static final String config_filename = "ows.json";
 	
 	public static final String default_version = "1.0.0";
 	
